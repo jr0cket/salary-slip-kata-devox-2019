e9b2d6b1217c5d6bbd8cce3da9e2f0a55478a17a
John Stevenson <john@jr0cket.co.uk>
2019-05-08 17:08:44 +0100

    Calculations are wrong, but approach seems okay
    Kept the code of scenario 1 quite simple.  Just returning a salary map using the
    data from the employee map passed as an argument.
    
    Missing data fields in the salary map are calculated as functions that are
    associated with the relevant keys.  Functions are evaluated before the salary
    map is returned.
    
    Where a function in the map uses existing values, we can use a let function to
    create a local name for the result of that function.  That name can be used for
    other functions in the map.

--- src/salary_slip_kata/core.clj
+++ src/salary_slip_kata/core.clj
@@ -9,7 +9,51 @@
 
 
 
+(defn- national-insurance-contribution
+  "Calculate the national insurance contribution due for a given annual salary.
+
+  ---------------------+-------------------------+--------
+  Band                 | NI deductible income    | NI Rate
+  ---------------------+-------------------------+--------
+  No contributions     | Up to £8,060.00         |    0%
+  Basic contributions  | £8,060.00 to £43,000.00 |   12%
+  Higher contributions | over £43,000.00         |    2%
+  ---------------------+-------------------------+-------- "
+
+  [annual-gross-salary]
+  ;; add a cond statement to return the calculate the value with respect to the band.
+  (* annual-gross-salary 0.12))
+
+
+;; taxable income
+;; ---------------------+---------------------------+---------
+;; Band                 | Taxable income            | Tax rate
+;; ---------------------+---------------------------+---------
+;; Personal Allowance*  | Up to £11,000.00          |    0%
+;; Basic rate           | £11,000.00 to £43,000.00  |   20%
+;; Higher rate          | £43,000.00 to £150,000.00 |   40%
+;; Additional rate      | over £150,000.00          |   45%
+;; ---------------------+---------------------------+---------
+
+
+
+
 (defn salary-slip
-  "Creates a salary slip for a person"
-  [employee]
-  (str "Show me the Mani !!"))
+  "Creates a salary slip for a person
+
+  Specifically for employee of 24K annual salary"
+
+  [{:keys [employee-id
+           employee-name
+           annual-gross-salary]}]
+  (let [tax-free-allowance 11000
+        taxable-income     (- annual-gross-salary
+                              tax-free-allowance)]
+    {:employee-id        employee-id
+     :employee-name      employee-name
+     :gross-salary       (/ annual-gross-salary 12)
+     :national-insurance (national-insurance-contribution annual-gross-salary)
+     :tax-free-allowance tax-free-allowance
+     :taxable-income     taxable-income
+     :tax-payable        (* taxable-income 0.20)}))
+
