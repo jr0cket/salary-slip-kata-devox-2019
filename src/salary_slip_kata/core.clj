cba53599d67cb1bda05676a6055771985fc5ece0
John Stevenson <john@jr0cket.co.uk>
2019-05-08 15:37:49 +0100

    salary-slip-test - failing
    Defined what an employee is
    Defined what a salary slip should look like
    
    Using map to represent the data of the Salary slip, to make the result simpler
    to test without intertwining a test of the output format.
    
    Added salary-slip function to enable compilation of the test.

--- src/salary_slip_kata/core.clj
+++ src/salary_slip_kata/core.clj
@@ -1,1 +1,15 @@
 (ns salary-slip-kata.core)
+
+;; Developer Anarchy by Fred George
+;; - made devs write the same solution in different languages
+;; -- helps devs master the business domain
+;; -- helps devs master technology domain
+
+;; Crazy book idea - 7 Emacs setups in 7 weeks
+
+
+
+(defn salary-slip
+  "Creates a salary slip for a person"
+  [employee]
+  (str "Show me the Mani !!"))
--- test/salary_slip_kata/core_test.clj
+++ test/salary_slip_kata/core_test.clj
@@ -1,3 +1,29 @@
 (ns salary-slip-kata.core-test
   (:require [clojure.test :refer :all]
             [salary-slip-kata.core :as sut]))
+
+;; employee has
+;; - Employee Id,
+;; - Employee Name
+;; - Annual Gross Salary
+
+;; example salary slip
+;; Employee ID: 12345
+;; Employee Name: John J Doe
+;; Gross Salary: £3750.00
+;; National Insurance contributions: £352.73
+;; Tax-free allowance: £916.67
+;; Taxable income: £2,833.33
+;; Tax payable: £600.00
+
+(deftest salary-slip-test
+  (let [employee [1233 "John J Doe" 24000]]
+    (testing "Produce a salary slip for a person"
+      (is (= {:employee-id        1233
+              :employee-name      "John J Doe"
+              :gross-salary       3730
+              :national-insurance 352.73
+              :tax-free-allowance 916.67
+              :taxable-income     2833.33
+              :tax-payable        600}
+             (sut/salary-slip employee))))))
